6aafef7e4198dc00fceb0efbb09f8ef62314b8b2
liaotao209 <liaotao209@pingan.com.cn>
2017-04-23 18:00:19 +0800

    update

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ echo execute : docker push ${REGISTRY_URL}app-${JOB_NAME}:${BUILD_NUMBER}
 docker push ${REGISTRY_URL}app-${JOB_NAME}:${BUILD_NUMBER}
 
 #Update existing container image(s) of resources
-#/usr/local/bin/kubectl set image deployment/k8stest-deployment k8stest=liaotao/app-${JOB_NAME}:${BUILD_NUMBER} --namespace=ci --kubeconfig=/usr/local/bin/config
+#/usr/local/bin/kubectl set image deployment/k8stest-deployment k8stest=liaotao/app-${JOB_NAME}:${BUILD_NUMBER} --namespace=default --kubeconfig=/Users/ethanliao/.kube/config
